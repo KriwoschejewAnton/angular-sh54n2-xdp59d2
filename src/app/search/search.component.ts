6809d00b560de240f5c3a5911a06ea68115e81f5
KriwoschejewAnton <akrivoshey@wiley.com>
2022-08-28 16:01:20 +0300

    1 file modified

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,7 +7,7 @@ import { SearchService } from './search.service';
   selector: 'app-search',
   templateUrl: './search.component.html',
   providers: [SearchService],
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
   heroes: Item[] = [];
@@ -15,9 +15,7 @@ export class SearchComponent implements OnInit {
   searchItem = '';
 
   constructor(private heroesService: SearchService) {
-
-//alert('SearchComponent');
-
+    //alert('SearchComponent');
   }
 
   @ViewChild('heroEditInput')
@@ -27,47 +25,35 @@ export class SearchComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-   
-  }
+  ngOnInit() {}
 
   search() {
     var searchTerm = this.searchItem;
     //alert(searchTerm);
-
 
     const myObserver = {
       next: () => alert('Observer got a next value: '),
       error: () => alert('Observer got an error: '),
       complete: () => alert('Observer got a complete notification'),
     };
-    
-
 
     this.editHero = undefined;
-    if (searchTerm) {
+    if (searchTerm.length > 3) {
       this.heroesService
         .searchHeroes(searchTerm)
         //.subscribe(myObserver);
-        .subscribe((heroes) => this.set(heroes) );
-
+        .subscribe((items) => this.set(items));
     } else {
-      
     }
   }
   set(items: Item[]) {
-    alert(
-      this.searchItem + " ### " + items[1].name 
-      );
+    alert(this.searchItem + ' ### ' + items[1].name);
   }
 
-  hide()
-  {
+  hide() {
     alert('hide');
   }
-
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
